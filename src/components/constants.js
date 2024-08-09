331f25ee2659d73d6a2dd5584dfcc91c908179c0
Paul Sojan <paul.eliassojan@gmail.com>
2024-08-09 11:40:48 +0530

    Misc fixes

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,11 +1,10 @@
 export const imageUrls = [
-    "https://ik.imagekit.io/d9mvewbju/SmileCart/thumbnail_61_7PaLfb.jpg",
-    "https://ik.imagekit.io/d9mvewbju/SmileCart/1_yuRfcETI5.jpg",
-    "https://ik.imagekit.io/d9mvewbju/SmileCart/2_HoKD2OblW.png",
-    "https://ik.imagekit.io/d9mvewbju/SmileCart/3_-6sHqwicC.png",
-  ];
-  export const VALID_COUNT_REGEX = /^(?:\d*|)$/;
-  export const MRP = "mrp";
-  export const OFFER_PRICE = "offerPrice";
-  export const CHECKOUT_LOCAL_STORAGE_KEY = "checkoutUserDetails";
-  
+  "https://ik.imagekit.io/d9mvewbju/SmileCart/thumbnail_61_7PaLfb.jpg",
+  "https://ik.imagekit.io/d9mvewbju/SmileCart/1_yuRfcETI5.jpg",
+  "https://ik.imagekit.io/d9mvewbju/SmileCart/2_HoKD2OblW.png",
+  "https://ik.imagekit.io/d9mvewbju/SmileCart/3_-6sHqwicC.png",
+];
+export const VALID_COUNT_REGEX = /^(?:\d*|)$/;
+export const MRP = "mrp";
+export const OFFER_PRICE = "offerPrice";
+export const CHECKOUT_LOCAL_STORAGE_KEY = "checkoutUserDetails";
